fcda53177ad2be600e53d8c5f5678b6c115dfce8
GrigoriyMo <kovarscky.g@yandex.ru>
2022-05-21 18:06:31 +0300

    test

---
+++ insert.sql
@@ -0,0 +1,5 @@
+--create user
+insert into "user"."user"
+("phone", "email","name")
+values 
+('+79111111114','hell2o@email.ru','DedInsider');
---
+++ procs.sql
@@ -0,0 +1,10 @@
+CREATE PROCEDURE "user".create_user(phone text, email text, name text)
+LANGUAGE SQL
+AS $$
+insert into "user"."user"
+("phone", "email","name")
+values 
+(phone,email,name);
+$$;
+
+call "user".create_user('+79111111116','hell4o@email.ru','DedInsider');
