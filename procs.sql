5aa2ec7b5409d668ffd0f83c161bef3d9f3f847f
GrigoriyMo <kovarscky.g@yandex.ru>
2022-05-21 18:32:18 +0300

    test

--- procs.sql
+++ procs.sql
@@ -8,3 +8,13 @@ values
 $$;
 
 call "user".create_user('+79111111116','hell4o@email.ru','DedInsider');
+
+
+CREATE PROCEDURE "user".create_confimation(phone text, email text, name text)
+LANGUAGE SQL
+AS $$
+insert into "user"."user"
+("phone", "email","name")
+values 
+(phone,email,name);
+$$;
